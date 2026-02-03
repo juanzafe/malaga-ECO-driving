396d38143812c4fe7e7bc95433a2e23445d18ab9
Juan Zamudio <juanzamudiofdez@gmail.com>
2026-02-03 12:09:53 +0100

    ci: add GitHub Actions workflow

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,14 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
+      prettierConfig,
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
